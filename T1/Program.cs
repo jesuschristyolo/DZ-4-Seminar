feedae3a25fe3beb7c275fcebdd9b9ac6f3f2c3c
Jesuschrist <valishev57@mail.ru>
2023-01-18 13:41:19 +0500

    1 Task Complete

---
+++ T1/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Input your number: ");
+int.TryParse(ReadLine(), out int number);
+
+
+WriteLine(Numsum(number));
+
+
+
+
+
+int Numsum(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    
+    }
+    return sum;
+}
