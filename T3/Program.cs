24ee5e396d7d676cc9c290eb022bd27e454f77e0
Jesuschrist <valishev57@mail.ru>
2023-01-19 01:09:53 +0500

    complete 3 task

---
+++ T3/Program.cs
@@ -0,0 +1,44 @@
+﻿using static System.Console;
+
+Clear();
+
+
+int[] arr = GetArray();
+int[] arr1 = new int [8];
+
+
+FullArray(arr1);
+PrintArray(arr1);
+
+
+
+void FullArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next (0, 99);
+    }
+}
+
+
+
+int [] GetArray ()
+{
+    int [] array = new int [8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0 , 99);
+    }
+    return array;
+}
+
+
+void PrintArray(int[] arr)
+{
+    
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        WriteLine($"{arr[i]}");
+    }
+}
