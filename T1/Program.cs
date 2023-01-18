60b048415819ae60917bd6639a68c2663564a702
Jesuschrist <valishev57@mail.ru>
2023-01-19 00:19:59 +0500

    comlete task1

--- T1/Program.cs
+++ T1/Program.cs
@@ -5,21 +5,33 @@ Clear();
 WriteLine("Input your number: ");
 int.TryParse(ReadLine(), out int number);
 
+int Length1 = NumberLen(number);
 
-WriteLine(Numsum(number));
-
-
-
+int Sum1 = NumberSum(Length1, number);
 
 
-int Numsum(int num)
+WriteLine($"Сумма цифр в числе {number}, составляет : {Sum1}");
+
+
+int NumberLen(int a)
+{
+    int count = 1;
+    while (a > 0)
+    {
+        a /= 10;
+        count++;
+    }
+    return count;
+}
+
+
+int NumberSum(int Length1, int num)
 {
     int sum = 0;
-    while (num > 0)
+    for (int i = 0; i <= num; i++)
     {
-        sum +=  num % 10;
+        sum += num %10;
         num /= 10;
-    
     }
     return sum;
 }
